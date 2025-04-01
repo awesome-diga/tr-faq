03d2c361b0ab0dd64970a1f6555983323fe9d0fa
Eduard Thamm <eduard.thamm@thammit.at>
2025-04-01 06:43:39 +0200

    Style footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,8 +36,8 @@ export default async function RootLayout({ children }) {
           {children}
           <Footer>
             <div className="flex justify-between w-full">
-              <div className="float-left">Content licensed under CC-BY-SA-4.0</div>
-              <div className="float-right"><Link href="/legal">Legal</Link></div>
+              <div className="float-left">Community content licensed under CC-BY-SA-4.0</div>
+              <div className="float-right text-pretty text-grey-600 dark:text-grey-400 text-decoration-line: underline"><a href="/legal" target="_blank">Rechtliches</a></div>
             </div>
           </Footer>
         </Layout>
